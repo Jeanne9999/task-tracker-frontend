963271c71e1e66490033082a24925c4fcbaa8916
Jeanne9999 <i.kotronaki@gmail.com>
2026-02-06 00:07:17 +0200

    chore: insert tasks mapping and demo data into Tasks page (for testing purpose)

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,6 +1,27 @@
 import Navbar from "../components/Navbar";
+import TaskCard from "../components/TaskCard";
+import type { Task } from "../types/task";
+
+
 
 const Tasks = () => {
+
+    const demoTasks: Task[] = [
+        {
+            id: 1,
+            title: "Learn Spring Security",
+            description: "Finish JWT authentication",
+            status: "TODO",
+        },
+        {
+            id: 2,
+            title: "React Dashboard",
+            description: "Build task UI",
+            status: "IN_PROGRESS",
+        },
+    ];
+
+
     return (
         <>
             <Navbar />
@@ -11,9 +32,19 @@ const Tasks = () => {
                     My Tasks
                 </h2>
 
-                <div className="bg-white shadow rounded p-4">
-                    No tasks yet...
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+                    {demoTasks.map((task) => (
+                        <TaskCard
+                            key={task.id}
+                            task={task}
+                            onEdit={(t) => console.log("Edit", t)}
+                            onDelete={(id) => console.log("Delete", id)}
+                        />
+                    ))}
+
                 </div>
+
 
             </div>
         </>
